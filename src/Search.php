d13eb8d79f0896b1c5db1e67db9715551c8f14a4
forg1v3n <aalissonrafael@yahoo.com>
2020-07-21 19:30:25 -0300

    Adiçao do PHP CodeSniffer

--- src/Search.php
+++ src/Search.php
@@ -2,10 +2,12 @@
 
 namespace forg1v3n\CepSearch;
 
-class Search {
+class Search
+{
     private $url = "https://viacep.com.br/ws/";
 
-    public function getAddressFromZipCode(string $zipCode): array{
+    public function getAddressFromZipCode(string $zipCode): array
+    {
         $zipCode = preg_replace('/[^0-9]/im', '', $zipCode);
 
         $get = file_get_contents($this->url . $zipCode . "/json");
